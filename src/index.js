c6a4299d056a828e14690743f2ab2d86984cec14
Chakib BOUABD <cbouabd@nomadvantage.com>
2022-08-13 23:23:39 +0200

    Add Jumbotron component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
+import { GlobalStyles } from './global-styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <>
+            <GlobalStyles />
+            <App />
+        </>
     </React.StrictMode>
 );
