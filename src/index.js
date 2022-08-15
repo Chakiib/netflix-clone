e9dbe0a8716684d4a128db843143a3470b29105f
Chakib BOUABD <cbouabd@nomadvantage.com>
2022-08-15 10:37:18 +0200

    Firebase base setup and init

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
+import { FirebseContext } from './context/firebase';
 import { GlobalStyles } from './global-styles';
+import { firestore } from './lib/firebase.prod';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
-        <>
+        <FirebseContext.Provider value={{ firestore }}>
             <GlobalStyles />
             <App />
-        </>
+        </FirebseContext.Provider>
     </React.StrictMode>
 );
