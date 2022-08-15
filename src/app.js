54a9211a14a726a671f73212c8042772d4b72750
Chakib BOUABD <chakib.bouabd@gmail.com>
2022-08-15 17:27:10 +0200

    Put back deleted app.js file (#8)
    Co-authored-by: Chakib BOUABD <cbouabd@nomadvantage.com>

---
+++ src/app.js
@@ -0,0 +1,54 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import { ProtectedRoute, PublicRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
+import { Browse, Home, SignIn, SingUp } from './pages';
+
+const App = () => {
+    const { user } = useAuthListener();
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    element={
+                        <PublicRoute user={user} loggedInPath={ROUTES.BROWSE}>
+                            <SignIn />
+                        </PublicRoute>
+                    }
+                    exact
+                    path={ROUTES.SIGN_IN}
+                />
+                <Route
+                    element={
+                        <PublicRoute user={user} loggedInPath={ROUTES.BROWSE}>
+                            <SingUp />
+                        </PublicRoute>
+                    }
+                    exact
+                    path={ROUTES.SIGN_UP}
+                />
+                <Route
+                    element={
+                        <ProtectedRoute user={user}>
+                            <Browse />
+                        </ProtectedRoute>
+                    }
+                    exact
+                    path={ROUTES.BROWSE}
+                />
+                <Route
+                    element={
+                        <PublicRoute user={user} loggedInPath={ROUTES.BROWSE}>
+                            <Home />
+                        </PublicRoute>
+                    }
+                    exact
+                    path={ROUTES.HOME}
+                />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default App;
